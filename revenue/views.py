7b981cb681e01867fc514bf173cfb4d868d768d2
Viktor Beniukh <superbigbenjamin@gmail.com>
2023-09-27 22:03:17 +0300

    Added views of spend and revenue apps

--- revenue/views.py
+++ revenue/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from django.db.models import Sum
+from revenue.models import RevenueStatistic
 
-# Create your views here.
+
+class RevenueStatisticView(APIView):
+    def get(self, request):
+        queryset = RevenueStatistic.objects.values("name", "date").annotate(
+            total_revenue=Sum("revenue"),
+            total_spend=Sum("spend__spend"),
+            total_impressions=Sum("spend__impressions"),
+            total_clicks=Sum("spend__clicks"),
+            total_conversion=Sum("spend__conversion"),
+        )
+        return Response(queryset)
--- spend/views.py
+++ spend/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from django.db.models import Sum
+from spend.models import SpendStatistic
 
-# Create your views here.
+
+class SpendStatisticView(APIView):
+    def get(self, request):
+        queryset = SpendStatistic.objects.values("name", "date").annotate(
+            total_spend=Sum("spend"),
+            total_impressions=Sum("impressions"),
+            total_clicks=Sum("clicks"),
+            total_conversion=Sum("conversion"),
+            total_revenue=Sum("revenue__revenue"),
+        )
+        return Response(queryset)
