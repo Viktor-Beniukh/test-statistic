c0aacb923da5f3be6f38de4dc54e93d7d79c57fb
Viktor Beniukh <superbigbenjamin@gmail.com>
2023-09-27 20:54:56 +0300

    Added rest_framework to settings.py and requirements.txt

--- statistic_service/settings.py
+++ statistic_service/settings.py
@@ -41,8 +41,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
     "spend",
-    "revenue"
+    "revenue",
 ]
 
 MIDDLEWARE = [
